4afd7ba95dac1e6159b22ddbf15d47873aec3f63
Daniel Shoemaker <danielshoe@charter.net>
2021-07-29 22:53:56 -0700

    Corrected math for hours

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ setInterval(function () {
 	var seconds = d.getSeconds() * 6 + 180;
 	var minutes = d.getMinutes() * 6 + 180;
 	var hours = d.getHours() * 30 + 180;
-	var hourPartial = hours / 8;
+	var hourPartial = hours;
+	var minuteRatio = (d.getMinutes() / 60) * 30 + hours;
 
-	console.log(hours);
+	console.log(
+		"Time: " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
+	);
 
 	document.getElementById("seconds").style.transform =
 		"rotate(" + seconds + "deg)";
@@ -14,5 +17,5 @@ setInterval(function () {
 		"rotate(" + minutes + "deg)";
 
 	document.getElementById("hour").style.transform =
-		"rotate(" + hourPartial + "deg)";
+		"rotate(" + minuteRatio + "deg)";
 }, 1000);
